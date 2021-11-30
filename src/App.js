9824cf03924ea30933e0a7d8f5e8a19c9801d787
Greg Testo <gregt@pursuit.org>
2021-11-30 13:49:13 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,88 @@
-import logo from './logo.svg';
+import React from 'react';
+import { Link, Route, BrowserRouter as Router, Switch } from 'react-router-dom';
 import './App.css';
 
-function App() {
+// Home
+// Contact
+// About Us
+// Career
+// Blog
+
+function HomePage(){
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      Home Page
+      <div>
+        <a href="/about">About Page</a>
+      </div>
+      <div>
+        <Link to="/about">About Page With Router</Link>
+      </div>
     </div>
-  );
+  )
+}
+
+function ContactPage(){
+  return (
+    <div>Contact Page</div>
+  )
+}
+
+function AboutUsPage(){
+  return (
+    <div>About Us</div>
+  )
+}
+
+function PetPage(props){
+  const pets = [
+    { id: "a234", name: "Oreo", species: "cat" },
+    { id: "a345", name: "Luna", species: "dog" },
+    { id: "b234", name: "Bell", species: "cat" }
+  ];
+  let choosenPet = pets.find((pet)=>{
+    return pet.id === props.match.params.id;
+  })
+
+  return (
+    <div>
+      <h1>Pet Page</h1>
+      
+      <div>Name: {choosenPet.name}</div>
+      <div>Species: {choosenPet.species}</div>
+    </div>
+  )
+}
+
+function App(){
+  return(    
+      <div>
+        {/* Navbar */}
+        <nav>
+          <Link to="/">Home</Link>
+          <Link to="/about">About Us</Link>
+          <Link to="/contact">Contact Us</Link>
+        </nav>
+        
+        {/* Routes */}
+        <Switch>
+          <Route exact path="/" component={HomePage} />
+          
+          <Route path="/about">
+            <AboutUsPage />
+          </Route>
+
+          <Route path="/contact">
+            <ContactPage />
+          </Route>
+
+          <Route path="/users/:id" component={PetPage} />
+          {/* <Route path="/users/:id">
+            <PetPage />
+          </Route> */}
+        </Switch>
+      </div>
+  )
 }
 
 export default App;
